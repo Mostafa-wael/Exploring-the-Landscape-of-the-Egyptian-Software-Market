74093d1ab05dd7fcb4140e8da59f4494a0c91705
abdullahalshawafi <abdullahadel.aam@gmail.com>
2023-05-12 23:28:06 +0300

    modify experience util to normalize its output

--- data/utils/experience.js
+++ data/utils/experience.js
@@ -1,5 +1,50 @@
 import cleanSpaces from "./cleanSpaces.js";
 
+export default function getExperience(html_root, filename) {
+    const experienceSection = html_root.querySelectorAll("section").filter((el) => el.querySelector("#experience"));
+
+    if (experienceSection.length > 0) {
+        const experienceUl = experienceSection[0].querySelector("ul");
+        const experienceLis = Array.from(experienceUl.querySelectorAll("li"));
+        const experienceArray = experienceLis.map((el) => getExperienceObject(el, filename)).filter((el) => el !== undefined);
+
+        const cleanExperienceArray = [];
+        experienceArray.forEach((experience) => {
+            if ("Positions" in experience) {
+                const positionsArray = experience.Positions.map((position) => {
+                    const obj = {
+                        User: experience.User,
+                        CompanyUrl: experience.CompanyUrl,
+                        Company: experience.Company,
+                        Title: position.Position,
+                        EmploymentType: position.EmploymentType ?? "",
+                        From: "",
+                        To: "",
+                        Duration: 0,
+                        Location: position.Location ?? "",
+                    }
+
+                    const { From, To, Duration } = parseFromToAndDuration(position.Date)
+                    obj.From = From;
+                    obj.To = To;
+                    obj.Duration = Duration;
+
+                    return obj;
+                });
+
+                cleanExperienceArray.push(...positionsArray);
+            } else {
+                cleanExperienceArray.push(experience);
+            }
+        });
+
+        return cleanExperienceArray;
+    }
+
+    return [];
+}
+
+
 function getExperienceObject(el, filename) {
     const containerDiv = el.querySelector("div");
 
@@ -11,7 +56,15 @@ function getExperienceObject(el, filename) {
     const haveMultiplePositions = secondDiv.querySelectorAll("div.pvs-list__outer-container>ul.pvs-list>li.pvs-list__item--one-column>span.pvs-entity__path-node").length > 0;
 
     const obj = {
-        "User": filename,
+        User: filename,
+        CompanyUrl: "",
+        Company: "",
+        Title: "",
+        EmploymentType: "",
+        From: "",
+        To: "",
+        Duration: 0,
+        Location: "",
     }
 
     if (firstDiv.querySelector("a")?.getAttribute("href") === undefined) {
@@ -39,10 +92,17 @@ function getExperienceObject(el, filename) {
             if (companyDateLocationSpan && companyDateLocationSpan.querySelector("span")) {
                 const companyDateLocation = companyDateLocationSpan.querySelector("span");
                 if (i === 0) {
-                    obj.Company = cleanSpaces(companyDateLocation.innerText);
+                    const companyAndEmploymentType = cleanSpaces(companyDateLocation.innerText);
+                    const { Company, EmploymentType } = parseCompanyAndEmploymentType(companyAndEmploymentType);
+                    obj.Company = Company;
+                    obj.EmploymentType = EmploymentType;
                 }
                 if (i === 1) {
-                    obj.Date = cleanSpaces(companyDateLocation.innerText);
+                    const fromToAndDuration = cleanSpaces(companyDateLocation.innerText);
+                    const { From, To, Duration } = parseFromToAndDuration(fromToAndDuration)
+                    obj.From = From;
+                    obj.To = To;
+                    obj.Duration = Duration;
                 }
                 if (i === 2) {
                     obj.Location = cleanSpaces(companyDateLocation.innerText);
@@ -80,11 +140,10 @@ function getExperienceObject(el, filename) {
             const otherSpans = positionDataDiv.querySelectorAll("span.t-normal.t-black--light");
             if (otherSpans.length === 2) {
                 positionObj.Date = cleanSpaces(otherSpans[0].querySelector("span").innerText);
-                positionObj.location = cleanSpaces(otherSpans[1].querySelector("span").innerText);
+                positionObj.Location = cleanSpaces(otherSpans[1].querySelector("span").innerText);
             } else if (otherSpans.length === 1) {
                 positionObj.Date = cleanSpaces(otherSpans[0].querySelector("span").innerText);
             }
-
 
             return positionObj;
         });
@@ -94,15 +153,78 @@ function getExperienceObject(el, filename) {
     }
 }
 
-export default function getExperience(html_root, filename) {
-    const experienceSection = html_root.querySelectorAll("section").filter((el) => el.querySelector("#experience"));
-
-    if (experienceSection.length > 0) {
-        const experienceUl = experienceSection[0].querySelector("ul");
-        const experienceLis = Array.from(experienceUl.querySelectorAll("li"));
-        const experienceArray = experienceLis.map((el) => getExperienceObject(el, filename)).filter((el) => el !== undefined);
-        return experienceArray;
+function parseCompanyAndEmploymentType(companyAndEmploymentType) {
+    const obj = {
+        Company: "",
+        EmploymentType: "",
     }
 
-    return [];
+    if (companyAndEmploymentType.includes(" · ")) {
+        const [companyName, employmentType] = companyAndEmploymentType.split(" · ");
+        obj.Company = companyName;
+        obj.EmploymentType = employmentType;
+    } else {
+        obj.Company = companyAndEmploymentType;
+    }
+
+    return obj;
+}
+
+function parseFromToAndDuration(fromToAndDuration) {
+    const obj = {
+        From: "",
+        To: "",
+        Duration: 0,
+    }
+
+    if (fromToAndDuration.includes(" · ")) {
+        let [fromTo, duration] = fromToAndDuration.split(" · ");
+        fromTo = parseFromTo(fromTo);
+        duration = parseDuration(duration);
+        obj.From = fromTo.From;
+        obj.To = fromTo.To;
+        obj.Duration = duration;
+    } else {
+        const fromTo = parseFromTo(fromToAndDuration);
+        obj.From = fromTo.From;
+        obj.To = fromTo.To;
+    }
+
+    return obj;
+}
+
+function parseFromTo(fromTo) {
+    const obj = {
+        From: "",
+        To: "",
+    }
+
+    if (fromTo.includes(" - ")) {
+        const [from, to] = fromTo.split(" - ");
+        obj.From = from;
+        obj.To = to;
+    } else {
+        obj.From = fromTo;
+    }
+
+    return obj;
+}
+
+function parseDuration(duration) {
+    const durationArray = duration.split(" ");
+    let years = 0;
+    let months = 0;
+
+    if (durationArray.length === 4) {
+        years = parseInt(durationArray[0]);
+        months = parseInt(durationArray[2]);
+    } else if (durationArray.length === 2) {
+        if (durationArray[1] === "yr" || durationArray[1] === "yrs") {
+            years = parseInt(durationArray[0]);
+        } else if (durationArray[1] === "mo" || durationArray[1] === "mos") {
+            months = parseInt(durationArray[0]);
+        }
+    }
+
+    return years * 12 + months;
 }
